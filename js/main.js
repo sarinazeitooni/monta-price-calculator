4f832d59afeaaeb7bbae2f22cac33f52491a4ef6
sarinazeitooni <76405658+sarinazeitooni@users.noreply.github.com>
2020-12-25 05:31:34 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,49 @@
 let price;
-let month;
-let fullprice = document.getElementById("fullprice");
+let item;
+let fullPrice = document.getElementById("fullprice");
 let percent70 = document.getElementById("percent70");
 let percent30 = document.getElementById("percent30");
 
-function discount(ID) {
-    let btn = document.getElementById("btn" + ID);
+function initialData(){
+    
+    percent70.innerHTML="17500";
+    fullPrice.innerHTML="25000";
+    percent30.innerHTML="7500";
+}
+
+function discount(firstDiscount,secondDiscount,ID) {
+    let button = document.getElementById("btn" + ID);
     let ico = document.getElementById("spn" + ID);
-    price = btn.getAttribute('data-price');
-    x = price * 30 / 100;
-    y = price * 70 / 100;
-    month = {
-        per70: y,
-        per30: x
+    price = button.getAttribute('data-price');
+    if(ID==6){
+       let percent7 = price - ((price*7)/100);
+        price = percent7;
+    }
+    firstPrice = (price * firstDiscount) / 100;
+    secondPrice = (price * secondDiscount) / 100;
+    
+    item = {
+        Percent70: secondPrice,
+        Percent30: firstPrice
     };
-    fullprice.innerHTML = price;
-    percent70.innerHTML = month.per70;
-    percent30.innerHTML = month.per30;
-    changer(btn, ico);
+    
+    fullPrice.innerHTML = price;
+    percent70.innerHTML = item.Percent70;
+    percent30.innerHTML = item.Percent30;
+    styleChanger(button, ico);
 };
 
-function changer(btn, ico) {
-    for (let i = 1; i < 6; i++) {
+function styleChanger(button, ico) {
+    for (let i = 1; i < 7; i++) {
         let x = "btn" + i;
         let y = "spn" + i;
         let yicon = document.getElementById(y);
         let xbtn = document.getElementById(x);
         do {
-            yicon.classList.remove("mi-mark-1");
-            ico.classList.add("mi-mark-1");
+            yicon.classList.remove("mi-Mark-2");
+            ico.classList.add("mi-Mark-2");
             xbtn.style.border = "1px #ebebeb solid";
-            btn.style.border = "1px #3fd200bb solid";
-        } while (x == btn && y == ico);
+            button.style.border = "1px #3fd200bb solid";
+        } while (x == button && y == ico);
     }
 };
